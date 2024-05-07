37878fae8cdf420267312790bb04bf375b468e7c
woojin-choi518 <woojin.choi518@gmail.com>
2024-05-07 10:26:22 +0900

    feat: UI

--- screens/Home.js
+++ screens/Home.js
@@ -8,7 +8,13 @@ import {
   StyleSheet,
   TouchableOpacity,
   BackHandler,
+  Dimensions,
+  SafeAreaView,
+  ImageBackground
 } from "react-native";
+
+const { width, height } = Dimensions.get('window'); // Get the screen width
+
 
 const Home = ({ navigation }) => {
   const [notification, setNotification] = useState(false);
@@ -61,91 +67,135 @@ const Home = ({ navigation }) => {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.textStyle}>SMUBUS</Text>
-
-      <View style={styles.card}>
-        <Text style={styles.noticeTitle}>공지사항</Text>
-        <Text style={styles.noticeContent}>{name}</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.textStyle}>SMUBUS</Text>
+        <View style={styles.card}>
+          <Text style={styles.noticeTitle}>공지사항</Text>
+          <Text style={styles.noticeContent}>{name}</Text>
+        </View>
       </View>
+      
+      <ImageBackground
+        source={require('../assets/roadf.png')}
+        resizeMode="contain"
+        style={styles.contentContainer}>
+        <TouchableOpacity 
+          style={styles.btncontainer}
+          onPress={() => goToScreen("Cheonan_Terminal")}>
+          <Text style={styles.item}>천안{"\n"}터미널</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => registerForPushNotifications()}>
-        <Text style={styles.item}>토큰생성</Text>
-      </TouchableOpacity>
+        <TouchableOpacity 
+          style={styles.btncontainer}
+          onPress={() => goToScreen("Cheonan_Station")}>
+          <Text style={styles.item}>천안역</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => goToScreen("Cheonan_Terminal")}>
-        <Text style={styles.item}>천안터미널</Text>
-      </TouchableOpacity>
+        <TouchableOpacity 
+          style={styles.btncontainer}
+          onPress={() => goToScreen("Cheonan_Asan_Station")}>
+          <Text style={styles.item}>천안{"\n"}아산역</Text>
+        </TouchableOpacity>
+      </ImageBackground>
+      <ImageBackground
+        source={require('../assets/roadf.png')}
+        resizeMode="contain" 
+        style={styles.contentContainer}>
+        <TouchableOpacity 
+          style={styles.btncontainer}
+          onPress={() => goToScreen("Onyang_Oncheon_Station")}>
+          <Text style={styles.item}> 온양온천역{"\n"}(아산터미널)</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => goToScreen("Cheonan_Station")}>
-        <Text style={styles.item}>천안역</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btncontainer}
+          onPress={() => goToScreen("Cheonan_Campus")}>
+          <Text style={styles.item}>천안{"\n"}캠퍼스</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => goToScreen("Cheonan_Asan_Station")}>
-        <Text style={styles.item}>천안아산역</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={() => goToScreen("Onyang_Oncheon_Station")}>
-        <Text style={styles.item}>온양온천역/아산터미널</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={() => goToScreen("Cheonan_Campus")}>
-        <Text style={styles.item}>천안캠퍼스</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={() => goToScreen("Screen2")}>
-        <Text style={styles.item}>시간표 바로가기</Text>
-      </TouchableOpacity>
-    </View>
+        <TouchableOpacity
+          style={styles.btncontainer}
+          onPress={() => goToScreen("Screen2")}>
+          <Text style={styles.item}> 시간표{"\n"}바로가기</Text>
+        </TouchableOpacity>
+      </ImageBackground>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#FFFFFF",
+    backgroundColor: "#f4f4f4",
     alignItems: "center",
     justifyContent: "center",
     paddingVertical: 20,
   },
-  title: {
-    fontSize: 26,
-    fontWeight: "bold",
-    marginBottom: 40,
+  header: {
+    flex:1,
+    backgroundColor: "#244092",
+    width: '100%', 
+    alignItems: 'center',
+    padding: 15,
+  },
+  contentContainer: {
+    flex: 2,
+    width: '100%',
+    backgroundColor: "#f4f4f4",
+    flexDirection: "row",
+    justifyContent: 'space-around', // 요소간 간격을 space-around로 변경하여 균등 배분
+    alignItems: 'center', // 모든 자식 요소를 세로 방향 가운데로 정렬
+    //padding: 15, // 패딩 조정
+  },
+  btncontainer: {
+    width: width * 0.28, // 버튼 크기 조정
+    height: height * 0.08,
+    borderRadius: 8, // 테두리 반경 조정
+    backgroundColor: "#eee",
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
   item: {
-    fontSize: 20,
-    marginBottom: 20,
+    fontSize: 18, // 폰트 크기 조정
+    fontWeight: "500", // 폰트 두께 조정
+    textAlign: "center"
   },
   textStyle: {
-    color: "black",
-    marginBottom: 20,
-    fontSize: 36,
-    fontWeight: "bold", // 텍스트의 두께를 조절합니다.
-  },
-  noticeContainer: {
-    marginBottom: 40,
-    alignItems: "center",
+    color: "#fff",
+    fontSize: 24, // 텍스트 크기 조정
+    fontWeight: "bold",
   },
   card: {
-    backgroundColor: "#EAEAEA",
-    borderRadius: 10,
+    backgroundColor: "#fff",
+    borderRadius: 8,
     padding: 20,
-    marginBottom: 30,
-    width: "80%",
+    marginTop:20,
+    width: "90%", // 카드의 너비를 늘려 콘텐츠가 더 잘 보이게 함
     alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
   },
   noticeTitle: {
     fontSize: 16,
     fontWeight: "bold",
-    marginBottom: 10,
-    color: "#555",
+    marginBottom: 5,
+    color: "#333", // 텍스트 색상 조정
   },
   noticeContent: {
     fontSize: 16,
-    color: "#777",
+    color: "#666", // 텍스트 색상을 더 진하게 조정
     textAlign: "center",
   },
 });
+
 
 export default Home;
--- screens/Splash.js
+++ screens/Splash.js
@@ -15,17 +15,10 @@ const Splash = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.textStyle}>선문 셔틀버스</Text>
-
       <Image
-        source={require("../assets/splash.png")}
+        source={require("../assets/splash_v3.png")}
         style={styles.imageStyle}
       />
-
-      <Text style={styles.subTextStyle}>
-        안전하고 편리한 셔틀버스을 이용하세요.
-      </Text>
-      <View style={{ height: windowHeight * 0.1 }} />
     </View>
   );
 };
@@ -33,10 +26,6 @@ const Splash = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#A9A9F5",
-    alignItems: "center",
-    justifyContent: "space-between", // 요소들 사이에 공간을 동일하게 분배합니다.
-    paddingTop: windowHeight * 0.1, // 상단 여백
   },
   textStyle: {
     color: "white",
@@ -44,14 +33,9 @@ const styles = StyleSheet.create({
     fontWeight: "bold", // 텍스트의 두께를 조절합니다.
   },
   imageStyle: {
-    width: windowWidth * 0.5, // 너비에 대해 유동적인 크기 조정
-    height: windowHeight * 0.3, // 높이에 대해 유동적인 크기 조정
-    resizeMode: "contain",
-  },
-  subTextStyle: {
-    fontSize: 18,
-    color: "white",
-    marginBottom: windowHeight * 0.1, // 하단 여백
+    width: windowWidth, // 너비에 대해 유동적인 크기 조정
+    height: windowHeight, // 높이에 대해 유동적인 크기 조정
+    resizeMode: "cover",
   },
 });
 
